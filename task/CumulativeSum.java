438b672c513247648a4140cd60cf3cf398d0275f
jrcarre <56210822+arrejohnray@users.noreply.github.com>
2024-12-02 03:43:26 +0000

    Task120

---
+++ task/CumulativeSum.java
@@ -0,0 +1,22 @@
+package task;
+
+public class CumulativeSum {
+
+    public static void getAllSum(int... nums){
+        int sum = 0;
+        for(int num : nums){
+            for(int i = 1; i <= num; i++){
+                sum += i;
+
+            }
+        }
+
+        System.out.println("The culminative sum is: " +sum);
+
+    }
+
+    public static void main(String[] args) {
+        getAllSum(4, 5,7);
+        
+    }
+}
